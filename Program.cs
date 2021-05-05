404cbce98b897ae9d1a07e69b210c52cb433f022
andersonfranciscoig@gmail.com <andersonfranciscoig@gmail.com>
2021-05-05 19:30:36 +0100

    inital coomit_

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace ingtegracaoTeste
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseUrls("http://*.1000");
+                    webBuilder.UseUrls("http://*.1001");
                     webBuilder.UseStartup<Startup>();
                 });
     }
